ed59d4c6d9af7d0797a9e4d09b28c1d5572eadf1
Gaspar <gabriel.sgaspar@yahoo.com.br>
2020-06-08 11:16:58 -0300

    create ResultFragment inflated by fragment_result layout

---
+++ app/src/main/java/com/example/bmi_app/result/ResultFragment.kt
@@ -0,0 +1,22 @@
+package com.example.bmi_app.result
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.bmi_app.R
+import com.example.bmi_app.databinding.FragmentResultBinding
+
+class ResultFragment: Fragment() {
+
+    private lateinit var binding: FragmentResultBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_result, container, false)
+        binding.lifecycleOwner = this
+
+        return binding.root
+    }
+}
